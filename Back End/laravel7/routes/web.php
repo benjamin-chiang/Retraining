2c52ed30f1ce2f638995d4a1a24ad8efe13b2c96
benjamin-chiang <76445870+benjamin-chiang@users.noreply.github.com>
2021-11-12 14:09:22 +0800

    1112

--- Back End/laravel7/routes/web.php
+++ Back End/laravel7/routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('test', function(){
+    $data = '123';
+    $data2 = '456';
+    $data3 = '789';
+    // 帶資料的方法
+    // 第一種：return view('test',['data'=>$data])
+    // 第二種：reutrn view('test')->with('data',$data)
+    // 第三種如下
+    // dd(compact('data','data2','data3'));
+    return view('test',compact('data','data2','data3'));
+});
+
+Route::get('lesson1', function () {
+
+    // $變數名稱
+    // = 指派運算子
+    $data = ['a'=>123, 'b'=>456]; // 陣列資料，'a'這是鍵 => 123這是值
+    // dd($data['a']); // 用方括號取值
+
+    $num1 = '5';
+    $num2 = '2';
+    // 算術運算子
+    // + - * / **平方 %
+    // 字串運算子「.」
+    dd($num1.$num2);
+
+    return 'hello world!';
 });
